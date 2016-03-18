ac4cd8ba31ab41ee2935f122079a193293a7010e
Pierre Charles <pierre.chls@gmail.com>
2016-03-18 15:40:43 +0100

    Add notifications and copy to clipboard function

---
+++ web/js/bttn-notif.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+  
+  /* Notification Share-URL-BTN */
+  
+  var bttn_share_url = document.getElementById( 'share-url' );
+  bttn_share_url.addEventListener( 'click', function() {
+  	setTimeout( function() {
+		// create the notification
+		var notification = new NotificationFx({
+			message : '<p>Partage !</p><div class="social"><a href="http://facebook.com" target="_blank"><img src="img/social/facebook.svg" /></a><a href="http://twitter.com" target="_blank"><img src="img/social/twitter.svg" /></a></div>',
+			layout : 'growl',
+			effect : 'scale',
+			type : 'notice', // notice, warning, error or success
+			ttl : 100000000
+		});
+		
+		// show the notification
+		notification.show();
+	}, 100 );
+  } );
+  
+  /* Notification Copy-URL-BTN */
+  
+  var bttn_copy_url = document.getElementById( 'copy-url' );
+  bttn_copy_url.addEventListener( 'click', function() {
+  	classie.add( bttn_copy_url, 'active' );
+	setTimeout( function() {
+		classie.remove( bttn_copy_url, 'active' );
+		// create the notification
+		var notification = new NotificationFx({
+			message : '<p>URL copiée !</p>',
+			layout : 'bar',
+			effect : 'slidetop',
+			type : 'warning', // notice, warning or error
+			onClose : function() {
+				bttn_copy_url.disabled = false;
+			}
+		});
+
+		// show the notification
+		notification.show();
+
+	}, 100 );
+					
+	// disable the button (for demo purposes only)
+	this.disabled = true;
+  } );
+  
+});
--- web/js/main.js
+++ web/js/main.js
@@ -29,11 +29,22 @@ $(document).ready(function(){
   		}	
   });
   
+  $("a#copy-url").click(function(event){
+  	event.preventDefault();
+  	$('html,body').animate({scrollTop:$(this.hash).offset()}, 800);
+    $("input#copyTarget").val($(location).attr('href'));
+    copyToClipboard(document.getElementById("copyTarget"));
+  });
+  
+  $("a#share-url").click(function(event){
+  	event.preventDefault();
+  	$('html,body').animate({scrollTop:$(this.hash).offset()}, 800);
+  });
+  
   
   $( window ).resize(function() {
   		menu_responsive();
   });
-  
   
   function menu_responsive(){
 	  if( $(window).width() > 992 || $(window).height() < 370 ){
@@ -41,5 +52,43 @@ $(document).ready(function(){
 		  $("#nav").removeClass("open");
 	  }
   }
+  
+  function copyToClipboard(elem) {
+	  // create hidden text element, if it doesn't already exist
+	    var targetId = "_hiddenCopyText_";
+	    var isInput = elem.tagName === "INPUT" || elem.tagName === "TEXTAREA";
+	    var origSelectionStart, origSelectionEnd;
+	    if (isInput) {
+	        // can just use the original source element for the selection and copy
+	        target = elem;
+	        origSelectionStart = elem.selectionStart;
+	        origSelectionEnd = elem.selectionEnd;
+	    }
+	    // select the content
+	    var currentFocus = document.activeElement;
+	    target.focus();
+	    target.setSelectionRange(0, target.value.length);
+	    
+	    // copy the selection
+	    var succeed;
+	    try {
+	    	  succeed = document.execCommand("copy");
+	    } catch(e) {
+	        succeed = false;
+	    }
+	    // restore original focus
+	    if (currentFocus && typeof currentFocus.focus === "function") {
+	        currentFocus.focus();
+	    }
+	    
+	    if (isInput) {
+	        // restore prior selection
+	        elem.setSelectionRange(origSelectionStart, origSelectionEnd);
+	    } else {
+	        // clear temporary content
+	        target.textContent = "";
+	    }
+	    return succeed;
+	}
   
 });
